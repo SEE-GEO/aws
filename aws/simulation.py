8ea7dbf0ed23a46c990f676c2648de54a3b88e8e
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-04-03 10:39:48 +0200

    Use customized abs settings.

--- aws/simulation.py
+++ aws/simulation.py
@@ -1,11 +1,22 @@
+import os
+
 from artssat import ArtsSimulation
 from artssat.atmosphere import Atmosphere1D
 from artssat.atmosphere.absorption import H2O, CloudWater, O2, N2
 from artssat.atmosphere.surface import Tessem
 from artssat.scattering.solvers import RT4
+from artssat.atmosphere.catalogs import LineCatalog
+from pyarts.workspace.api import include_path_push
 
 from aws.hydrometeors import Ice, Rain
 from aws.sensor import AWS
+
+try:
+    aws_path = os.path.dirname(__file__)
+except:
+    aws_path = "."
+include_path_push(os.path.join(aws_path, "include"))
+
 
 class Simulation(ArtsSimulation):
     def __init__(self,
@@ -18,7 +29,14 @@ class Simulation(ArtsSimulation):
         scattering_solver = RT4(nstreams=32)
         atmosphere = Atmosphere1D(absorbers=absorbers,
                                   scatterers=scatterers,
-                                  surface=surface)
+                                  surface=surface,
+                                  catalog=LineCatalog("abs_lines_h2o_rttov.xml"))
+
+
+        self.includes = ["general/general.arts",
+                         "include_mpm89_cont.arts",
+                         "general/planet_earth.arts"]
+
         super().__init__(atmosphere=atmosphere,
                          data_provider=data_provider,
                          sensors=[sensor],
