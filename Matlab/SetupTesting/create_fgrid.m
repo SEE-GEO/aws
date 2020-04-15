05526b59edbbbb68c5943660670965c42c66b973
Patrick Eriksson <patrick.eriksson@chalmers.se>
2020-04-15 15:51:52 +0200

    Work on selecting frequencies.

---
+++ Matlab/SetupTesting/create_fgrid.m
@@ -0,0 +1,31 @@
+% Compiles the f_grid to apply in ARTS
+%
+% FORMAT f_grid = create_fgrid
+
+% 2020-04-15 Patrick Eriksson
+
+function f_grid = create_fgrid
+
+r        = 0.5;
+n_window = 1;
+n_wing   = 5;
+
+[f_grid,f_lims,f_chs,f0] = test_f_input( '89' );
+f89 = test_f_interp(f_grid,f_lims,f_chs,r,n_window,n_window);
+
+[f_grid,f_lims,f_chs,f0] = test_f_input( '166' );
+f166 = test_f_interp(f_grid,f_lims,f_chs,r,n_window,n_window);
+
+[f_grid,f_lims,f_chs,f0] = test_f_input( '229' );
+f229 = test_f_interp(f_grid,f_lims,f_chs,r,n_window,n_window);
+
+[f_grid,f_lims,f_chs,f0] = test_f_input( '183l' );
+f183l = test_f_interp(f_grid,f_lims,f_chs,r,n_wing,n_wing);
+
+[f_grid,f_lims,f_chs,f0] = test_f_input( '325l' );
+f325l = test_f_interp(f_grid,f_lims,f_chs,r,n_wing,n_wing);
+
+[f_grid,f_lims,f_chs,f0] = test_f_input( '325u' );
+f325u = test_f_interp(f_grid,f_lims,f_chs,r,n_wing,n_wing);
+
+f_grid = [f89,f166,f183l,f229,f325l,f325u]';
--- Matlab/SetupTesting/test_f_interp.m
+++ Matlab/SetupTesting/test_f_interp.m
@@ -1,24 +1,50 @@
-function f_opt = test_f_interp(f_grid,f_lims,f_chs,r_surface,nmax,nout)
+% Testing of frequencies to apply
+%
+% FORMAT f_n = test_f_interp(f_grid,f_lims,f_chs,r_surface,nmax,nout)
+%
+% OUT   f_n      Frequency grid applied for nout
+% IN    f_grid   Fine grid. Data on this grid represents the truth. Should
+%                cover all other frequencies.
+%       f_lims   Frequency limits for equidistant sparse grid.
+%       f_chs    Edge frequencies for each channel to evaluate. One row per channel.
+%       r_surface  Surface reflectivity.
+%       nmax     Max points in sparse grid to consider
+%       nout     Return f_n and make plot for this n.
+
+% 2020-04-15 Patrick Eriksson
+
+function f_n = test_f_interp(f_grid,f_lims,f_chs,r_surface,nmax,nout)
 
 %- Get reference spectrum
 %
 Y0 = calc_atms( f_grid, r_surface );
 
 
+%- Special case of empty f_lims
+%
+if isempty(f_lims)
+  [nstart,nmax,nout] = deal( size(f_chs,1) );
+else
+  nstart = 1;
+end
+    
+
 %- Test with different n
 %
-fprintf('\n             Full band         Channel\n')
-fprintf(' n    mean      std      mex      max\n')
-fprintf('-------------------------------------\n')
+fprintf('\n             Full band                   Channel\n')
+fprintf(' n    mean      std      max     mean      std      max\n')
+fprintf('-------------------------------------------------------\n')
 %
-for n = 1 : nmax
-  if n == 1
+for n = nstart : nmax
+  if isempty(f_lims)
+    f_test    = mean( f_chs, 2 )';
+  elseif n == 1
     f_test   = (f_lims(1) + f_lims(end)) / 2;
   else
     f_test   = linspace( f_lims(1), f_lims(end), n );
   end
   [Y,atms] = calc_atms( f_test, r_surface );
-  if 1
+  if 0
     imethod = 'spline';
     if n == 1
       dY      = repmat( Y, length(f_grid), 1 );
@@ -44,24 +70,28 @@ for n = 1 : nmax
   dY     = dY - Y0;
   %
   if n == nout
-    f_opt = f_test;
-    plot( f_grid/1e9, dY );
-    grid on;
-    xlabel( 'Frequency [GHz]' );
-    ylabel( 'Interpolatio error [K]' ); 
-    title( sprintf('%s with %d points',stitle,n) );
-    legend( upper(atms), 'Location', 'South' );
-    drawnow;
+    f_n = f_test;
+    if ~nargout
+      plot( f_grid/1e9, dY );
+      grid on;
+      xlabel( 'Frequency [GHz]' );
+      ylabel( 'Interpolatio error [K]' ); 
+      title( sprintf('%s with %d points',stitle,n) );
+      legend( upper(atms), 'Location', 'South' );
+      drawnow;
+    end
   end
   %
   dY = dY(:);
   %
-  fprintf( '%02d: %+4.2fK / %5.2fK / %5.2fK / %5.2fK\n', ...
-           n, mean(dY), std(dY), max(abs(dY)), max(abs(Ce(:))) );
+  fprintf( '%02d: %+4.2fK / %5.2fK / %5.2fK / %5.2fK / %5.2fK / %5.2fK\n', ...
+           n, mean(dY), std(dY), max(abs(dY)), ...
+           mean(Ce(:)), std(Ce(:)), max(abs(Ce(:))) );
 
 end
 
-fprintf('-------------------------------------\n')
+fprintf('-------------------------------------------------------\n')
+
 
 return
 
