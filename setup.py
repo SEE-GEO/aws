dd6c9e0d2c6030aa315607bf8c4919c64a7d66e5
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-09-24 20:15:36 +0200

    Added definitions of quantile loss and CRPS.

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
     url='https://github.com/simonpf/aws',  # Optional
-    author='Simon Pfreundschuh, Patrick Eriksson, Inderpreet Kaur',  # Optional
+    author='Simon Pfreundschuh'
     author_email='simon.pfreundschuh@chalmers.se',  # Optional
-    install_requires=["artssat"],
+    install_requires=["scikitimage", "requests", "appdirs"],
     packages=["aws"],
     python_requires='>=3.6',
     project_urls={  # Optional
